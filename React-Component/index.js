2f395bdb3fc3e28f9022d5863cf2a0cc1c5a75f3
Devishanmugam18 <79523505+Devishanmugam18@users.noreply.github.com>
2023-05-23 15:38:06 +0530

    Update index.js

--- React-Component/index.js
+++ React-Component/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-function Component() {
-  return <h1>I am a react component</h1>
+import React from 'react'; // importing react dependencies from react
+import ReactDom from 'react-dom'; //importing react-dom which enables to utilize 'render' method
+function Component() { //creating component
+  return <h1>I am a react component</h1> //must return
 }
 
 ReactDom.render(<Component/>,document.getElementById('root'));
